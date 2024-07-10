23de1bc0a22b7a34f1f8fe4954ff38ea0d5a5c76
GabiiF <gabriela.fernandez.92@live.com.ar>
2024-07-10 03:02:16 -0300

    le oy estilos al banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,8 @@
 import './Banner.css'
 
 const Banner = ()=>{
-    return <div> Prueba e</div>
+    return <div className='banner'>
+        <img src="/img/banner.png" alt="Aluraflix" />
+    </div>
 }
 export default Banner
